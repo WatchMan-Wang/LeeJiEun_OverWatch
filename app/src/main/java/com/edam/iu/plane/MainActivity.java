20a841e9a6532219997fff314cd414ab6cb32b33
ShouWang <514504765@qq.com>
2022-02-10 17:49:24 +0800

    LeeJiEun include TapTap login and TapTap AntiAddiction

---
+++ app/src/main/java/com/edam/iu/plane/MainActivity.java
@@ -0,0 +1,287 @@
+package com.edam.iu.plane;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.edam.iu.plane.game.GameView;
+import com.edam.iu.plane.tools.StatusBarUtil;
+import com.tapsdk.antiaddiction.config.AntiAddictionFunctionConfig;
+import com.tapsdk.antiaddiction.constants.Constants;
+import com.tapsdk.antiaddictionui.AntiAddictionUICallback;
+import com.tapsdk.antiaddictionui.AntiAddictionUIKit;
+import com.tapsdk.bootstrap.Callback;
+import com.tapsdk.bootstrap.TapBootstrap;
+import com.tapsdk.bootstrap.account.TDSUser;
+import com.tapsdk.bootstrap.exceptions.TapError;
+import com.tapsdk.moment.TapMoment;
+import com.taptap.sdk.TapLoginHelper;
+import com.tds.common.entities.TapConfig;
+import com.tds.common.entities.TapDBConfig;
+import com.tds.common.models.TapRegionType;
+
+import java.util.Map;
+
+import cn.leancloud.LCLogger;
+import cn.leancloud.LeanCloud;
+import cn.leancloud.json.JSON;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "LeeJiEun";
+    private TextView textView;
+    private int count = 3;
+    private Animation animation;
+    private Button btnTapLogin;
+    private Button btnInGame;
+    private LinearLayout linearLayout;
+    private String userID = "";
+    TDSUser currentUser = null;
+    String userIdentifier = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initStatusBar();
+
+        initView();
+
+        initTapSDK();
+
+
+    }
+
+    private void initView() {
+        textView = (TextView) findViewById(R.id.textView);
+        btnTapLogin = (Button) findViewById(R.id.btn_tap_login);
+        btnInGame = (Button) findViewById(R.id.btn_ingame);
+        linearLayout = (LinearLayout) findViewById(R.id.ll_text);
+
+        btnTapLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                taptapLogin();
+            }
+        });
+
+        btnInGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                enterGame();
+            }
+        });
+
+        animation = AnimationUtils.loadAnimation(this, R.anim.countdown_animation);
+        handler.sendEmptyMessageDelayed(0, 1000);
+    }
+
+    private void enterGame() {
+        // 登陆成功后跳转
+        Intent intent = new Intent(getApplicationContext(), GameActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void initTapSDK() {
+        LeanCloud.setLogLevel(LCLogger.Level.DEBUG);
+        WebView.setWebContentsDebuggingEnabled(true);
+        // 内建账户方式登陆 SDK 初始化
+        // TapDB 初始化
+        TapDBConfig tapDBConfig = new TapDBConfig();
+        tapDBConfig.setEnable(true);
+        tapDBConfig.setChannel("gameChannel");
+        tapDBConfig.setGameVersion("gameVersion");
+        // TapSDK 初始化
+        TapConfig tapConfig = new TapConfig.Builder()
+                .withAppContext(getApplicationContext())
+                .withRegionType(TapRegionType.CN) // TapRegionType.CN: 国内  TapRegionType.IO: 国外
+                // 自己账号 - 知恩
+                .withClientId("FwFdCIr6u71WQDQwQN")
+                .withClientToken("8zkWbrNMBXYtdg6GTyGy3FLRcIi1C5PuKjxwWAUe")
+                .withServerUrl("https://fwfdcir6.cloud.tds1.tapapis.cn")
+                .withTapDBConfig(tapDBConfig)
+                .build();
+        TapBootstrap.init(MainActivity.this, tapConfig);
+
+        // 注册动态回调监听
+        TapMoment.setCallback(new TapMoment.TapMomentCallback() {
+            @Override
+            public void onCallback(int code, String msg) {
+                if (code == TapMoment.CALLBACK_CODE_GET_NOTICE_SUCCESS) {
+                    // 获取用户新消息成功
+                    Toast.makeText(MainActivity.this, "获取新通知数据为： " + msg, Toast.LENGTH_SHORT).show();
+                }
+                if (code == TapMoment.CALLBACK_CODE_LOGIN_SUCCESS) {
+                    // 动态内登陆成功
+                    Toast.makeText(MainActivity.this, "动态内登陆成功： " + msg, Toast.LENGTH_SHORT).show();
+                }
+                if (code == TapMoment.CALLBACK_CODE_PUBLISH_SUCCESS) {
+                    // 动态发布成功
+                    Toast.makeText(MainActivity.this, "动态发布成功： " + msg, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        // Android SDK 的各接口第一个参数是当前 Activity，以下不再说明
+        String gameIdentifier = "FwFdCIr6u71WQDQwQN";
+        AntiAddictionFunctionConfig config = new AntiAddictionFunctionConfig.Builder()
+                .enablePaymentLimit(true) // 是否启用消费限制功能
+                .enableOnLineTimeLimit(true) // 是否启用时长限制功能
+                .build();
+        AntiAddictionUIKit.init(MainActivity.this, gameIdentifier, config,
+                new AntiAddictionUICallback() {
+                    @Override
+                    public void onCallback(int code, Map<String, Object> extras) {
+                        // 根据 code 不同提示玩家不同信息，详见下面的说明
+                        if (null != extras) {
+                            Log.d(TAG, extras.toString());
+                            Log.d(TAG, String.valueOf(code));
+                        }
+                        switch (code) {
+                            case Constants.ANTI_ADDICTION_CALLBACK_CODE.LOGIN_SUCCESS:
+//                                Log.d(TAG, extras.toString());
+                                Log.d(TAG, "防沉迷登陆成功");
+                                Toast.makeText(MainActivity.this, "防沉迷认证成功，进入游戏", Toast.LENGTH_SHORT).show();
+                                enterGame();
+                                break;
+                            case Constants.ANTI_ADDICTION_CALLBACK_CODE.LOGOUT:
+//                                Log.d(TAG, extras.toString());
+                                Log.d(TAG, "防沉迷的登出");
+                                Toast.makeText(MainActivity.this, "防沉迷的登出", Toast.LENGTH_SHORT).show();
+                                break;
+                            case Constants.ANTI_ADDICTION_CALLBACK_CODE.OPEN_ALERT_TIP:
+                                Log.d(TAG, "防沉迷未成年允许游戏弹窗");
+                                Toast.makeText(MainActivity.this, "防沉迷未成年允许游戏弹窗", Toast.LENGTH_SHORT).show();
+                                break;
+                            case Constants.ANTI_ADDICTION_CALLBACK_CODE.NIGHT_STRICT:
+                                Log.d(TAG, "防沉迷未成年玩家无法进行游戏");
+                                Toast.makeText(MainActivity.this, "防沉迷未成年玩家无法进行游戏", Toast.LENGTH_SHORT).show();
+                                break;
+                            case Constants.ANTI_ADDICTION_CALLBACK_CODE.REAL_NAME_STOP:
+                                Log.d(TAG, "防沉迷实名认证过程中点击了关闭实名窗");
+                                Toast.makeText(MainActivity.this, "防沉迷实名认证过程中点击了关闭实名窗", Toast.LENGTH_SHORT).show();
+                                break;
+                            case Constants.ANTI_ADDICTION_CALLBACK_CODE.SWITCH_ACCOUNT:
+                                Log.d(TAG, "防沉迷实名认证过程中点击了切换账号按钮");
+                                Toast.makeText(MainActivity.this, "防沉迷实名认证过程中点击了切换账号按钮", Toast.LENGTH_SHORT).show();
+                                break;
+                        }
+                    }
+                }
+        );
+    }
+
+    private void tapAntiAddiction() {
+        if (userIdentifier == "") {
+            Toast.makeText(MainActivity.this, "用户唯一标识为空，检查 Tap 授权", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        AntiAddictionUIKit.startup(MainActivity.this, true, userIdentifier);
+    }
+
+    private void taptapLogin() {
+        TDSUser.loginWithTapTap(MainActivity.this, new Callback<TDSUser>() {
+            @Override
+            public void onSuccess(TDSUser resultUser) {
+                // 开发者可以调用 resultUser 的方法获取更多属性。
+                userID = resultUser.getObjectId();
+
+                String userName = resultUser.getUsername();
+                String avatar = (String) resultUser.get("avatar");
+                Log.d(TAG, "userID: " + userID);
+                Log.d(TAG, "userName: " + userName);
+                Log.d(TAG, "avatar: " + avatar);
+                Map<String, Object> authData = (Map<String, Object>) resultUser.get("authData");
+                Map<String, Object> taptapAuthData = (Map<String, Object>) authData.get("taptap");
+                Log.d(TAG, "authData:" + JSON.toJSONString(authData));
+                Map<String, Object> authDataResult = (Map<String, Object>) ((Map<String, Object>) resultUser.get("authData")).get("taptap");
+                Log.d(TAG, "unionid:" + taptapAuthData.get("unionid").toString());
+                Log.d(TAG, "openid:" + taptapAuthData.get("openid").toString());
+                userIdentifier = taptapAuthData.get("openid").toString();
+                Toast.makeText(MainActivity.this, "succeed to login with Taptap.", Toast.LENGTH_SHORT).show();
+
+                tapAntiAddiction();
+            }
+
+            @Override
+            public void onFail(TapError error) {
+                Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+                Log.d(TAG, error.detailMessage);
+                Log.d(TAG, error.getMessage());
+                Log.d(TAG, error.toJSON());
+            }
+        }, TapLoginHelper.SCOPE_PUBLIC_PROFILE);
+    }
+
+    private int getCount() {
+        count--;
+        if (count == 0) {
+            Log.d(TAG, "******* handler *******");
+            handler.removeMessages(0);
+            linearLayout.setVisibility(View.GONE);
+            currentUser = TDSUser.getCurrentUser();
+            if (currentUser == null) {
+                btnInGame.setVisibility(View.GONE);
+                btnTapLogin.setVisibility(View.VISIBLE);
+            } else {
+                btnInGame.setVisibility(View.VISIBLE);
+                btnTapLogin.setVisibility(View.GONE);
+            }
+        }
+        return count;
+    }
+
+    @SuppressLint("HandlerLeak")
+    private Handler handler = new Handler() {
+        public void handleMessage(android.os.Message msg) {
+            if (msg.what == 0) {
+                textView.setText(getCount() + "");
+                handler.sendEmptyMessageDelayed(0, 1000);
+                animation.reset();
+                textView.startAnimation(animation);
+            }
+        }
+
+        ;
+    };
+
+    private void initStatusBar() {
+        //沉浸式代码配置
+        //当FitsSystemWindows设置 true 时，会在屏幕最上方预留出状态栏高度的 padding
+        StatusBarUtil.setRootViewFitsSystemWindows(this, false);
+        //设置状态栏透明
+        StatusBarUtil.setTranslucentStatus(this);
+        //一般的手机的状态栏文字和图标都是白色的, 可如果你的应用也是纯白色的, 或导致状态栏文字看不清
+        //所以如果你是这种情况,请使用以下代码, 设置状态使用深色文字图标风格, 否则你可以选择性注释掉这个if内容
+        if (!StatusBarUtil.setStatusBarDarkTheme(this, true)) {
+            //如果不支持设置深色风格 为了兼容总不能让状态栏白白的看不清, 于是设置一个状态栏颜色为半透明,
+            //这样半透明+白=灰, 状态栏的文字能看得清
+            StatusBarUtil.setStatusBarColor(this, 0x55000000);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeMessages(0);
+    }
+
+}
